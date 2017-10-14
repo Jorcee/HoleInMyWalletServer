c807690b38f039fff53ef69e461405a4968498d8
Jordan Goldstein <jfgold0401@gmail.com>
2017-10-14 07:49:36 -0400

    Mostly functions with Azure

--- CreateMerchantCSV.py
+++ CreateMerchantCSV.py
@@ -5,21 +5,29 @@ import json
 import csv
 import ast
 users = pd.read_json('http://api.reimaginebanking.com/merchants?key=0f030ef091420133d187099758681c3a&page=',typ='series')
-for i in range(2,236):
+for i in range(2,10):
     users['data'] += pd.read_json('http://api.reimaginebanking.com/merchants?key=0f030ef091420133d187099758681c3a&page='+str(i),typ='series')['data']
 thing = json_normalize(users['data'][1])
 merchant = pd.DataFrame.from_dict(thing)
-for i in range(2,4000):
+for i in range(1,50):
     thing = json_normalize(users['data'][i])
     merchant = merchant.append(pd.DataFrame.from_dict(thing))
 merchant.to_csv("users.csv",index=False,header=False)
 oldCSV = pd.read_csv("users.csv")
+merchantCSV = oldCSV[oldCSV.columns[0]]
 oldCSV.drop(oldCSV.columns[[1,2,3,4,5,6,7,9,10,11]],axis=1,inplace=True)
 newCSV = pd.DataFrame()
 tagList = []
+merchantList = []
+for row in oldCSV.iterrows():
+    merchantList.append(row[0])
 for row in oldCSV.iterrows():
     tags = ast.literal_eval(row[1][1])
+    tags = ["Type=" + x for x in tags]
     tagList.append(tags)
+merchantCSV.to_csv("debug.csv",index=False,header=False)
 newCSV = pd.DataFrame(tagList)
-newCSV.insert(0,'ID',oldCSV.columns[0])
+newCSV.insert(0,'ID',merchantCSV)
+for i in range(0,49):
+    newCSV.iloc[i] = newCSV.iloc[i].str.upper()
 newCSV.to_csv("Merchants.csv",index=False,header=False)
